519f3f4b8fc029ad68d73cf040eff6c68d2df3a9
Ilkhom Kh <ukhuseinov1@gmail.com>
2024-05-22 18:04:34 +0500

    created DdatePicker class

--- tests/HomeWorkTest.spec.ts
+++ tests/HomeWorkTest.spec.ts
@@ -26,19 +26,36 @@ import { test as it } from '@playwright/test'
   it.describe("HOME TYPE", () => {
     it("Fill all fields", async ({page}) => {
 
-        await page.goto("https://demoqa.com/automation-practice-form",);
+      //  await page.goto("http://localhost:4200/pages/forms/layouts",);
+      //   await page.locator('[placeholder="Jane Doe"]').fill('Ashurchik2')
+      //   // await page.locator('[placeholder="Jane Doe"] ~ [placeholder="Email"]').fill('not_fully_works@gmail.com')
+      //   await page.locator("//form[@class='form-inline ng-untouched ng-pristine ng-valid']/input[2]").fill('not_fully_works@gmail.com')
+      //   /*await page.locator('[placeholder="Jane Doe"] ~ [class="status-basic nb-transition"]').click()*/
+      //   /* await page.locator('[placeholder="Jane Doe"] ~ [type="submit"]').click()*/
+      //   await page.locator("//*[@id='inputEmail1']").fill('not_fully_works@gmail.com')
+      //   await page.locator("//*[@id='inputPassword2']").fill('1234test')
+      //   await page.locator("//*[contains(text(),'Option 1')]").click()
+      //   await page.locator("//*[contains(text(),'Sign in')][@class='appearance-filled size-medium shape-rectangle status-primary nb-transition']").click()*/
 
+        
+        
+             
+
+
+        await page.goto("http://demoqa.com/automation-practice-form",);
         await page.locator('[autocomplete="off"][class=" mr-sm-2 form-control"][id="firstName"]').fill("Ashur");
-                 await page.locator("#lastName").fill("ITishnikov");
-        await page.locator("#lastName").pressSequentially("ITishnikov", { delay: 200 })
+        await page.locator("#lastName").fill("ITishnikov");
+        // await page.locator("#lastName").pressSequentially("ITishnikov", { delay: 200 })
         await page.locator('[autocomplete="off"][class="mr-sm-2 form-control"][id="userEmail"]').fill("email@email.com")
                 // await page.locator('[autocomplete="off"][class="mr-sm-2 form-control"][id="userEmail"]').pressSequentially("email@email.com", { delay: 200 })
         await page.locator('[autocomplete="off"][class=" mr-sm-2 form-control"][id="userNumber"]').fill("1234567890")
-        await page.locator('[for="gender-radio-1"]').click();
-                await page.locator('[for="gender-radio-1"][class="custom-control-label"]').click("")
+        await page.locator('[for="gender-radio-1"][class="custom-control-label"]').click();
+        await page.locator('//*[contains(text(),"Submit")]').click();
 
-        // await page.getByPlaceholder('Select State').fill('Rajasthan')
+        // await page.getByPlaceholder('[class=" css-1wa3eu0-placeholder"],("Select State")').fill('Rajasthan')
         
 
     });
   });
+
+
